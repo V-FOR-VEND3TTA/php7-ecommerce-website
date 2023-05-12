6f81de0071e781f96ff64c1c72e9d89d9d52833b
George Hlongwane <georgehlongwane8@gmail.com>
2023-05-12 08:58:31 +0200

    Created index file, added Bootstrap, and added navigation

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--Bootstrap-->
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<!--Custom CSS-->
+	<link rel="stylesheet" type="text/css" href="main/css/main.css">
+	<title>Anavrin</title>
+</head>
+<body>
+	<!--<h1>Anavrin</h1>-->
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<a href="index.php" class="navbar-brand">Anavrin</a>
+			<ul class="nav navbar-nav">
+				
+			</ul>
+		</div>
+	</nav>
+	<script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
