c2e0f7fee82f5ffdbc942e13b95a91fec4d1936e
George Hlongwane <georgehlongwane8@gmail.com>
2023-05-13 20:35:32 +0200

    Added a dropdown menu to the navigation bar

--- index.php
+++ index.php
@@ -4,21 +4,40 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!--Bootstrap-->
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap-5.3.0-alpha3-dist/css/bootstrap.min.css">
 	<!--Custom CSS-->
-	<link rel="stylesheet" type="text/css" href="main/css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<title>Anavrin</title>
 </head>
 <body>
-	<!--<h1>Anavrin</h1>-->
-	<nav class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-			<a href="index.php" class="navbar-brand">Anavrin</a>
-			<ul class="nav navbar-nav">
-				
-			</ul>
-		</div>
-	</nav>
-	<script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- to go back to the original light background<nav class="navbar navbar-expand-lg navbar-light bg-light" >-->
+	<!--An override of the background color-->
+	<nav class="navbar navbar-expand-lg navbar-light" id="navbar">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php" id="text">Anavrin</a>
+    <!--Dropdown-->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon caret"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <button class="btn btn-light dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" id="text">
+            Men
+          </button>
+          <!--Dropdown items-->
+          <ul class="dropdown-menu dropdown-menu" role="menu">
+            <li><a class="dropdown-item" href="#">Shirts</a></li>
+            <li><a class="dropdown-item" href="#">Pants</a></li>
+            <li><a class="dropdown-item" href="#">Shoes</a></li>
+            <li><a class="dropdown-item" href="#">Accessories</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+	<!--Bootstrap's JavaScript-->
+	<script type="text/javascript" src="bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
