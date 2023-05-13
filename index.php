999ffe788d8bb218f862fa75c3c13256c37095cf
George Hlongwane <georgehlongwane8@gmail.com>
2023-05-13 21:55:46 +0200

    Inserted images in our background

--- index.php
+++ index.php
@@ -37,6 +37,18 @@
     </div>
   </div>
 </nav>
+<!--Inserting images-->
+<div id="background-image">
+	<div id="image-1"></div>
+	<div id="image-2"></div>	
+</div>
+<p>
+	Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum nesciunt suscipit, tempore tempora, laudantium ipsa non hic necessitatibus. Aut, reprehenderit beatae dolores ab, deleniti labore neque omnis architecto sint repellat.
+	Lorem, ipsum dolor sit amet consectetur, adipisicing elit. Nam exercitationem et repellat, aliquid mollitia facilis accusantium maxime labore nesciunt corporis fugit velit harum fuga eaque earum nemo distinctio molestias perspiciatis.
+	Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo officia reprehenderit earum quae sint at cumque nostrum id debitis, ratione sapiente adipisci quaerat qui laborum quibusdam expedita delectus quidem fugiat?<br>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque odit doloremque iste quo dolorem repellendus vitae harum suscipit ut praesentium fuga, excepturi, provident dolorum quos eaque sed voluptas maxime odio. Ipsum atque fugiat beatae aliquid maiores voluptatem nulla quas libero voluptatibus, cum quis, nisi provident, rerum impedit accusamus! Beatae, aliquam.<br>
+	Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, eius eveniet molestias rerum error, soluta omnis quidem dicta quam fugiat illo quasi, ut delectus fugit. Fugiat atque libero quidem aliquam suscipit delectus quia, itaque, assumenda, illum at unde ullam ratione autem impedit. Quod temporibus, molestias maxime illo totam quo incidunt, sequi reiciendis dolorum cupiditate libero. Modi velit sint repellat unde aliquam voluptate, praesentium sequi itaque. Blanditiis, non. Quasi a similique molestias sequi dolorum dolor, dicta debitis ad voluptates, enim minima adipisci atque deleniti est deserunt! Animi est nam laudantium blanditiis, eligendi quia rem error officiis, molestias labore! Quae, velit, doloremque?
+</p>
 	<!--Bootstrap's JavaScript-->
 	<script type="text/javascript" src="bootstrap-5.3.0-alpha3-dist/js/bootstrap.bundle.min.js"></script>
 </body>
